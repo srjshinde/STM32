99fbaaf48fdece8c2acc674d9fc1ac223b654242
srjshinde <41166426+srjshinde@users.noreply.github.com>
2019-09-30 17:38:28 +0530

    Updates touchscreen pin assignments

--- TFT_ILI9341_STM32_arduino/Adafruit_ILI9341_8bit_STM-master/Adafruit_ILI9341_8bit_STM.h
+++ TFT_ILI9341_STM32_arduino/Adafruit_ILI9341_8bit_STM-master/Adafruit_ILI9341_8bit_STM.h
@@ -99,7 +99,7 @@ Define pins and Output Data Registers
 //Port data |D7 |D6 |D5 |D4 |D3 |D2 |D1 |D0 |
 //Pin stm32 |PA7|PA6|PA5|PA4|PA3|PA2|PC1|PA0|
 //Control pins |RD |WR |RS |CS |RST|
-//Pin stm32    |PB4|PB5|PB6|PB7|PB8|
+//Pin stm32    |PB4|PB5|PB0|PB1|PB8|
 #define TFT_CNTRL      GPIOB
 #define TFT_DATA       GPIOA
 #define TFT_RD         PB4
